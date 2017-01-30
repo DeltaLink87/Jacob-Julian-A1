25dec02a26388eb7e93df927bf2584718d6a50d7
EpicLink87 <jacob@martinkids.ca>
2017-01-30 10:06:46 -0500

    Update PurchaseArray.h

--- PurchaseArray.h
+++ PurchaseArray.h
@@ -1,12 +1,22 @@
 #ifndef PURCHASEARRAY_H
 #define PURCHASEARRAY_H
 
+#include "Product.h"
+#include "Purchase.h"
+#include <string>
+#include "defs.h"
+
 class PurchaseArray{
   public:
     PurchaseArray();
-  
+    void addPurchase(Product&, int);
+    Product& getPurchaseByName(string);
+    Product& getPurchaseByID(int);
+    void printArray();
+    
   private:
-
+    Purchase purchases[MAX_ARR];
+    int size;
 };
 
 #endif
