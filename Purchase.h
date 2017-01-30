a64b557ff6c6be667d6f187ba47b76ce43e44f47
EpicLink87 <jacob@martinkids.ca>
2017-01-30 10:06:14 -0500

    Update Purchase.h

--- Purchase.h
+++ Purchase.h
@@ -1,12 +1,19 @@
 #ifndef PURCHASE_H
 #define PURCHASE_H
 
+#include "Product.h"
+
 class Purchase{
   public:
     Purchase();
-  
+    Purchase(Product&, int);
+    int getQuantity();
+    void setQuantity(int);
+    Product& getProduct();
+    
   private:
-
+    Product product;
+    int quantity;
 };
 
 #endif
